d57b2a0ff5057347bca3507b2d64df654a857fc5
Mala Ruparel <mala.ruparel@smartsensesolutions.com>
2024-04-07 14:00:10 +0530

    Initial Commit

---
+++ app/src/main/java/com/ss/storagepermission/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.ss.storagepermission
+
+import android.graphics.drawable.BitmapDrawable
+import android.net.Uri
+import android.os.Bundle
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import com.ss.storagepermission.databinding.ActivityMain1Binding
+
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMain1Binding
+
+}
